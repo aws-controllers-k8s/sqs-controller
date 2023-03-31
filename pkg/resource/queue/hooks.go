8a54ff1f7177e5b304f92d34ee24660deec9a8fe
Jay Pipes <jaypipes@gmail.com>
2023-03-31 11:44:28 -0700

    Adds ability to update tags for SQS Queues (#24)
    Brings in the the ability to update and delete tags from an SQS Queue
    using the code hook points recently added to the sdkUpdate method for
    SetAttributes-based APIs like SNS/SQS.
        
    Issue aws-controllers-k8s/community#1541
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

---
+++ pkg/resource/queue/hooks.go
@@ -0,0 +1,134 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//     http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package queue
+
+import (
+	"context"
+
+	ackrtlog "github.com/aws-controllers-k8s/runtime/pkg/runtime/log"
+	svcsdk "github.com/aws/aws-sdk-go/service/sqs"
+)
+
+// syncTags examines the Tags in the supplied Queue and calls the
+// ListQueueTags, TagQueue and UntagQueue APIs to ensure that the set of
+// associated Tags  stays in sync with the Queue.Spec.Tags
+func (rm *resourceManager) syncTags(
+	ctx context.Context,
+	desired *resource,
+	latest *resource,
+) (err error) {
+	rlog := ackrtlog.FromContext(ctx)
+	exit := rlog.Trace("rm.syncTags")
+	defer func() { exit(err) }()
+	toAdd := map[string]*string{}
+	toDelete := []*string{}
+
+	existingTags := latest.ko.Spec.Tags
+
+	for k, v := range desired.ko.Spec.Tags {
+		if ev, found := existingTags[k]; !found || *ev != *v {
+			toAdd[k] = v
+		}
+	}
+
+	for k, _ := range existingTags {
+		if _, found := desired.ko.Spec.Tags[k]; !found {
+			deleteKey := k
+			toDelete = append(toDelete, &deleteKey)
+		}
+	}
+
+	if len(toAdd) > 0 {
+		for k, v := range toAdd {
+			rlog.Debug("adding tag to queue", "key", k, "value", *v)
+		}
+		if err = rm.addTags(ctx, desired, toAdd); err != nil {
+			return err
+		}
+	}
+	if len(toDelete) > 0 {
+		for _, k := range toDelete {
+			rlog.Debug("removing tag from queue", "key", *k)
+		}
+		if err = rm.removeTags(ctx, desired, toDelete); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// getTags returns the list of tags to the Queue
+func (rm *resourceManager) getTags(
+	ctx context.Context,
+	r *resource,
+) (map[string]*string, error) {
+	var err error
+	var resp *svcsdk.ListQueueTagsOutput
+	rlog := ackrtlog.FromContext(ctx)
+	exit := rlog.Trace("rm.getTags")
+	defer func() { exit(err) }()
+
+	input := &svcsdk.ListQueueTagsInput{}
+	input.QueueUrl = r.ko.Status.QueueURL
+
+	// NOTE(jaypipes): Unlike many other ListTags APIs, SQS's is not
+	// paginated...
+	resp, err = rm.sdkapi.ListQueueTagsWithContext(ctx, input)
+	rm.metrics.RecordAPICall("READ_MANY", "ListQueueTags", err)
+	if err != nil || resp == nil {
+		return nil, err
+	}
+	// and the output's Tags field is actually a map[string]*string... go
+	// figure :)
+	return resp.Tags, err
+}
+
+// addTags adds the supplied Tags to the supplied Queue resource
+func (rm *resourceManager) addTags(
+	ctx context.Context,
+	r *resource,
+	tags map[string]*string,
+) (err error) {
+	rlog := ackrtlog.FromContext(ctx)
+	exit := rlog.Trace("rm.addTag")
+	defer func() { exit(err) }()
+
+	input := &svcsdk.TagQueueInput{}
+	input.QueueUrl = r.ko.Status.QueueURL
+	input.Tags = tags
+
+	_, err = rm.sdkapi.TagQueueWithContext(ctx, input)
+	rm.metrics.RecordAPICall("UPDATE", "TagQueue", err)
+	return err
+}
+
+// removeTags removes the supplied Tags from the supplied Queue resource
+func (rm *resourceManager) removeTags(
+	ctx context.Context,
+	r *resource,
+	tagKeys []*string, // the set of tag keys to delete
+) (err error) {
+	rlog := ackrtlog.FromContext(ctx)
+	exit := rlog.Trace("rm.removeTag")
+	defer func() { exit(err) }()
+
+	input := &svcsdk.UntagQueueInput{}
+	input.QueueUrl = r.ko.Status.QueueURL
+	input.TagKeys = tagKeys
+
+	_, err = rm.sdkapi.UntagQueueWithContext(ctx, input)
+	rm.metrics.RecordAPICall("UPDATE", "UntagQueue", err)
+	return err
+}
--- test/e2e/tests/test_queue.py
+++ test/e2e/tests/test_queue.py
@@ -14,84 +14,114 @@
 """Integration tests for the Queue API.
 """
 
-import boto3
 import pytest
 import time
 import logging
-from typing import Dict, Tuple
 
 from acktest.resources import random_suffix_name
-from acktest.aws.identity import get_region
 from acktest.k8s import resource as k8s
+from acktest import tags
 from e2e import service_marker, CRD_GROUP, CRD_VERSION, load_sqs_resource
 from e2e.replacement_values import REPLACEMENT_VALUES
 from e2e.bootstrap_resources import get_bootstrap_resources
+from e2e import sqsqueue
 
 RESOURCE_PLURAL = "queues"
 
 CREATE_WAIT_AFTER_SECONDS = 5
-UPDATE_WAIT_AFTER_SECONDS = 5
-DELETE_WAIT_AFTER_SECONDS = 60
+MODIFY_WAIT_AFTER_SECONDS = 10
+DELETE_WAIT_AFTER_SECONDS = 20
+
+@pytest.fixture(scope="module")
+def simple_queue():
+    resource_name = random_suffix_name("sqs-queue", 24)
+
+    resources = get_bootstrap_resources()
+    logging.debug(resources)
+
+    replacements = REPLACEMENT_VALUES.copy()
+    replacements["QUEUE_NAME"] = resource_name
+
+    # Load Queue CR
+    resource_data = load_sqs_resource(
+        "queue",
+        additional_replacements=replacements,
+    )
+    logging.debug(resource_data)
+
+    # Create k8s resource
+    ref = k8s.CustomResourceReference(
+        CRD_GROUP, CRD_VERSION, RESOURCE_PLURAL,
+        resource_name, namespace="default",
+    )
+    k8s.create_custom_resource(ref, resource_data)
+    cr = k8s.wait_resource_consumed_by_controller(ref)
+
+    sqsqueue.wait_until_exists(resource_name)
+
+    yield cr, ref
+
+    # Delete k8s resource
+    _, deleted = k8s.delete_custom_resource(
+        ref,
+        period_length=DELETE_WAIT_AFTER_SECONDS,
+    )
+    assert deleted
+
+    sqsqueue.wait_until_deleted(resource_name)
+
 
 @service_marker
 @pytest.mark.canary
 class TestQueue:
-    def get_queue(self, sqs_client, queue_name: str) -> dict:
-        try:
-            resp = sqs_client.get_queue_url(
-                QueueName=queue_name
-            )
-            return resp
-
-        except Exception as e:
-            logging.debug(e)
-            return None
-
-    def queue_exists(self, sqs_client, queue_name: str) -> bool:
-        return self.get_queue(sqs_client, queue_name) is not None
-
-    def test_smoke(self, sqs_client):
-        resource_name = random_suffix_name("sqs-queue", 24)
-
-        resources = get_bootstrap_resources()
-        logging.debug(resources)
-
-        replacements = REPLACEMENT_VALUES.copy()
-        replacements["QUEUE_NAME"] = resource_name
-        replacements["AWS_REGION"] = get_region()
-
-        # Load Queue CR
-        resource_data = load_sqs_resource(
-            "queue",
-            additional_replacements=replacements,
-        )
-        logging.debug(resource_data)
-
-        # Create k8s resource
-        ref = k8s.CustomResourceReference(
-            CRD_GROUP, CRD_VERSION, RESOURCE_PLURAL,
-            resource_name, namespace="default",
-        )
-        k8s.create_custom_resource(ref, resource_data)
-        cr = k8s.wait_resource_consumed_by_controller(ref)
-
-        assert cr is not None
-        assert k8s.get_resource_exists(ref)
+    def test_crud(self, simple_queue):
+        res, ref = simple_queue
 
         time.sleep(CREATE_WAIT_AFTER_SECONDS)
 
-        cr = k8s.wait_resource_consumed_by_controller(ref)
+        # Update the tags associated with the Queue and verify the update is
+        # reflected in the SQS API
+        cr = k8s.get_resource(ref)
+        assert cr is not None
+        assert 'spec' in cr
+        assert 'delaySeconds' in cr['spec']
+        assert cr['spec']['delaySeconds'] == "0"
+        assert 'status' in cr
+        assert 'queueURL' in cr['status']
+        queue_url = cr['status']['queueURL']
 
-        # Check Queue exists
-        exists = self.queue_exists(sqs_client, resource_name)
-        assert exists
+        latest_attrs = sqsqueue.get_attributes(queue_url)
+        assert 'DelaySeconds' in latest_attrs
+        assert latest_attrs['DelaySeconds'] == "0"
 
-        # Delete k8s resource
-        _, deleted = k8s.delete_custom_resource(ref)
-        assert deleted is True
+        # Test updating tags...
+        assert 'tags' in cr['spec']
+        assert len(cr['spec']['tags']) == 1
+        assert 'key1' in cr['spec']['tags']
+        assert cr['spec']['tags']['key1'] == 'val1'
 
-        time.sleep(DELETE_WAIT_AFTER_SECONDS)
+        expect_before_update_tags = {
+            "key1": "val1",
+        }
+        latest_tags = sqsqueue.get_tags(queue_url)
+        tags.assert_equal_without_ack_tags(
+            expect_before_update_tags, latest_tags,
+        )
 
-        # Check Queue doesn't exist
-        exists = self.queue_exists(sqs_client, resource_name)
-        assert not exists
+        new_tags = {
+            "key1": None, # Unfortunately, this is the only way to remove a key
+            "key2": "val2",
+        }
+        updates = {
+            "spec": {"tags": new_tags},
+        }
+        k8s.patch_custom_resource(ref, updates)
+        time.sleep(MODIFY_WAIT_AFTER_SECONDS)
+
+        expect_after_update_tags = {
+            "key2": "val2",
+        }
+        latest_tags = sqsqueue.get_tags(queue_url)
+        tags.assert_equal_without_ack_tags(
+            expect_after_update_tags, latest_tags,
+        )
