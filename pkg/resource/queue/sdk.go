66b8d79ed73a3d9926064f6971b98d22a7fbb339
Yu-Lang Chu <yu.chu@deliveryhero.com>
2023-12-21 08:43:19 +0100

    add comments

--- pkg/resource/queue/sdk.go
+++ pkg/resource/queue/sdk.go
@@ -293,8 +293,9 @@ func (rm *resourceManager) sdkUpdate(
 		return nil, err
 	}
 	// note(Julian-Chu): SetAttributes API without any attributes
-	// will return MalformedInput(message: End of list found where not expected) error. We need to set minimal one default value,
-	// if there are no attributes in the input
+	// will return MalformedInput(message: End of list found where not expected) error.
+	// if there are no attributes in the input,
+	// We need to set minimal one default value, or use customUpdate to skip api call.
 	if len(input.Attributes) == 0 {
 		input.Attributes = map[string]*string{
 			"DelaySeconds": latest.ko.Spec.DelaySeconds,
