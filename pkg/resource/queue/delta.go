c994e8524faa9eadb3d1223b073925d64d43e8a4
Michael Tewoldemedhin <98621731+michaelhtm@users.noreply.github.com>
2025-03-27 12:20:26 -0700

    Prepare repository for tag conversion function signature update (#73)
    Description of changes:
    The To/FromACKTags function signatures are changing with the next
    code-gen release. This PR aims to ensure custom code adapts as well.
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- pkg/resource/queue/delta.go
+++ pkg/resource/queue/delta.go
@@ -140,7 +140,9 @@ func newResourceDelta(
 			delta.Add("Spec.SQSManagedSSEEnabled", a.ko.Spec.SQSManagedSSEEnabled, b.ko.Spec.SQSManagedSSEEnabled)
 		}
 	}
-	if !ackcompare.MapStringStringEqual(ToACKTags(a.ko.Spec.Tags), ToACKTags(b.ko.Spec.Tags)) {
+	desiredACKTags, _ := convertToOrderedACKTags(a.ko.Spec.Tags)
+	latestACKTags, _ := convertToOrderedACKTags(b.ko.Spec.Tags)
+	if !ackcompare.MapStringStringEqual(desiredACKTags, latestACKTags) {
 		delta.Add("Spec.Tags", a.ko.Spec.Tags, b.ko.Spec.Tags)
 	}
 	if ackcompare.HasNilDifference(a.ko.Spec.VisibilityTimeout, b.ko.Spec.VisibilityTimeout) {
--- templates/hooks/queue/sdk_get_attributes_post_set_output.go.tpl
+++ templates/hooks/queue/sdk_get_attributes_post_set_output.go.tpl
@@ -1,5 +1,5 @@
 	if tags, err := rm.getTags(ctx, r); err != nil {
 		return nil, err
 	} else {
-		ko.Spec.Tags = FromACKTags(tags)
+		ko.Spec.Tags = fromACKTags(tags, nil)
 	}
